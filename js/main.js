49607ff141503706b7b34fbbb4b3369a7c8b887b
MATVEN <mattiavena2993@gmail.com>
2019-10-28 16:50:35 +0100

    add com function

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@
 //Generiamo un numero random (sempre da 1 a 5) per il computer.
 //Sommiamo i due numeri e dichiariamo chi ha vinto.
 
-// palindroma
-
 // pari o dispari
-var x = prompt('Scegli un numero pari(0) o un numero dispari(1).');
+var x = prompt('Scegli un numero pari o un numero dispari.');
 
 // condition
-if (x == 0) {
+if (x % 2 == 0) {
   console.log('Pari!');
 } else {
   console.log('Dispari!');
 }
 
 var z = parseInt(prompt('Scegli un numero da 1 a 5.'));
+console.log(z);
+
+var tot = sum();
+console.log(tot);
+var winner = pariDispari();
+
+function sum() {
+  var com = Math.floor(Math.Random() * 6 + 1);
+  console.log(com);
+  result = z + com;
+
+  return result;
+}
