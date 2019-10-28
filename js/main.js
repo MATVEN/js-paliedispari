7ea32cd8f2cdaac292052416abccf2e54539c3d1
MATVEN <mattiavena2993@gmail.com>
2019-10-28 16:29:50 +0100

    add var and condition

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Una funzione per capire se la parola è palindroma
+//L’utente sceglie pari o dispari e un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer.
+//Sommiamo i due numeri e dichiariamo chi ha vinto.
+
+// palindroma
+
+// pari o dispari
+var x = prompt('Scegli un numero pari(0) o un numero dispari(1).');
+
+// condition
+if (x == 0) {
+  console.log('Pari!');
+} else {
+  console.log('Dispari!');
+}
+
+var z = parseInt(prompt('Scegli un numero da 1 a 5.'));
